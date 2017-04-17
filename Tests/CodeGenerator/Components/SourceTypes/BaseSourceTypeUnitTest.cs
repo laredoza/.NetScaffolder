af672bce6d78e3295434b2be827370d2d2e9f973
Laredo Tirnanic <laredoza@gmail.com>
2017-04-17 21:18:25 +0200

    #11 #19 #31 Added Tests for Customer

--- Tests/CodeGenerator/Components/SourceTypes/BaseSourceTypeUnitTest.cs
+++ Tests/CodeGenerator/Components/SourceTypes/BaseSourceTypeUnitTest.cs
@@ -28,6 +28,9 @@ namespace DotNetScaffolder.Test.Components.SourceTypes
 
             this.BaseSourceTypeUnitTest_SoftwareTable(databaseModel.Tables.FirstOrDefault(t => t.TableName == "Software"));
             this.BaseSourceTypeUnitTest_Software(databaseModel.Tables.FirstOrDefault(t => t.TableName == "Software"));
+
+            this.BaseSourceTypeUnitTest_CustomerTable(databaseModel.Tables.FirstOrDefault(t => t.TableName == "Customer"));
+            this.BaseSourceTypeUnitTest_CustomerRelationship(databaseModel.Tables.FirstOrDefault(t => t.TableName == "Customer"));
         }
 
 
@@ -200,6 +203,103 @@ namespace DotNetScaffolder.Test.Components.SourceTypes
             Assert.AreEqual(RelationshipType.ForeignKey, relationship.DependencyRelationShip, "The OrderDetails DependencyRelationShip should be Child in the Product table.");
             Assert.AreEqual("ProductId", relationship.ForeignColumnName, "The OrderDetails ForeignColumnName should be ProductId in the Product table.");
             Assert.AreEqual("Product", relationship.TableName, "The relationship table name should be Product");
+        }
+
+        [TestMethod]
+        public void BaseSourceTypeUnitTest_CustomerTable(Table customerTable)
+        {
+            Assert.IsNotNull(customerTable, "The table should not be null");
+            Assert.AreEqual(13, customerTable.Columns.Count, "There should be 13 Columns in the Software table.");
+
+            Column column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "CustomerId");
+            Assert.IsTrue(column.IsPrimaryKey, "The CustomerId should be a primary key in the CustomerTable table.");
+            Assert.AreEqual(DomainDataType.Int32, column.CSharpDataType, "The CustomerId should be a int32 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "CustomerCode");
+            Assert.IsFalse(column.IsPrimaryKey, "The CustomerCode is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The LicenseCode should be a string in the Customer table.");
+            Assert.AreEqual(5, column.Length, "The CustomerCode should have a length of 5 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "CompanyName");
+            Assert.IsFalse(column.IsPrimaryKey, "The CompanyName is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The CompanyName should be a string in the Customer table.");
+            Assert.AreEqual(50, column.Length, "The CompanyName should have a length of 50 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "ContactName");
+            Assert.IsFalse(column.IsPrimaryKey, "The ContactName is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The ContactName should be a string in the Customer table.");
+            Assert.AreEqual(50, column.Length, "The ContactName should have a length of 50 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "ContactTitle");
+            Assert.IsFalse(column.IsPrimaryKey, "The ContactTitle is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The ContactTitle should be a string in the Customer table.");
+            Assert.AreEqual(50, column.Length, "The ContactTitle should have a length of 50 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "Address");
+            Assert.IsFalse(column.IsPrimaryKey, "The Address is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The Address should be a string in the Customer table.");
+            Assert.AreEqual(50, column.Length, "The Address should have a length of 50 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "City");
+            Assert.IsFalse(column.IsPrimaryKey, "The City is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The City should be a string in the Customer table.");
+            Assert.AreEqual(20, column.Length, "The City should have a length of 20 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "PostalCode");
+            Assert.IsFalse(column.IsPrimaryKey, "The PostalCode is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The PostalCode should be a string in the Customer table.");
+            Assert.AreEqual(10, column.Length, "The PostalCode should have a length of 10 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "Telephone");
+            Assert.IsFalse(column.IsPrimaryKey, "The Telephone is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The Telephone should be a string in the Customer table.");
+            Assert.AreEqual(50, column.Length, "The Telephone should have a length of 50 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "Fax");
+            Assert.IsFalse(column.IsPrimaryKey, "The Fax is not a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The Fax should be a string in the Customer table.");
+            Assert.AreEqual(50, column.Length, "The Fax should have a length of 50 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "CountryId");
+            Assert.IsFalse(column.IsPrimaryKey, "The CountryId should be a primary key in the CustomerTable table.");
+            Assert.AreEqual(DomainDataType.Int32, column.CSharpDataType, "The CountryId should be a int32 in the Customer table.");
+
+            // Todo: Change to correct type
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "Photo");
+            Assert.IsFalse(column.IsPrimaryKey, "The Photo should be a primary key in the Customer table.");
+            Assert.AreEqual(DomainDataType.String, column.CSharpDataType, "The Photo should be a int32 in the Customer table.");
+
+            column = customerTable.Columns.FirstOrDefault(c => c.ColumnName == "IsEnabled");
+            Assert.IsFalse(column.IsPrimaryKey, "The IsEnabled should be a primary key in the CustomerTable table.");
+            Assert.AreEqual(DomainDataType.Boolean, column.CSharpDataType, "The IsEnabled should be a boolean in the Customer table.");
+        }
+
+        [TestMethod]
+        public void BaseSourceTypeUnitTest_CustomerRelationship(Table softwareTable)
+        {
+            Assert.IsNotNull(softwareTable.RelationShips, "The Relationships should not be null in the Customer table.");
+            Assert.AreEqual(3, softwareTable.RelationShips.Count, "There should be 3 relationships in the Customer table.");
+
+            Relationship relationship = softwareTable.RelationShips.FirstOrDefault(r => r.TableName == "BankAccount");
+            Assert.IsNotNull(relationship, "The BankAccount Relationship should not be null.");
+            Assert.AreEqual("CustomerId", relationship.ColumnName, "The ColumnName should be CustomerId.");
+            Assert.AreEqual(RelationshipType.ForeignKeyChild, relationship.DependencyRelationShip, "The OrderDetails DependencyRelationShip should be ForeignKeyChild.");
+            Assert.AreEqual("CustomerId", relationship.ForeignColumnName, "The ForeignColumnName should be CustomerId.");
+            Assert.AreEqual("BankAccount", relationship.TableName, "The relationship table name should be BankAccount");
+
+            relationship = softwareTable.RelationShips.FirstOrDefault(r => r.TableName == "Country");
+            Assert.IsNotNull(relationship, "The Country Relationship should not be null.");
+            Assert.AreEqual("CountryId", relationship.ColumnName, "The ColumnName should be CountryId.");
+            Assert.AreEqual(RelationshipType.ForeignKey, relationship.DependencyRelationShip, "The DependencyRelationShip should be ForeignKey.");
+            Assert.AreEqual("CountryId", relationship.ForeignColumnName, "The ForeignColumnName should be CountryId.");
+            Assert.AreEqual("Country", relationship.TableName, "The relationship table name should be Country.");
+
+            relationship = softwareTable.RelationShips.FirstOrDefault(r => r.TableName == "Order");
+            Assert.IsNotNull(relationship, "The Order Relationship should not be null.");
+            Assert.AreEqual("CustomerId", relationship.ColumnName, "The ColumnName should be CustomerId.");
+            Assert.AreEqual(RelationshipType.ForeignKeyChild, relationship.DependencyRelationShip, "The OrderDetails DependencyRelationShip should be ForeignKeyChild.");
+            Assert.AreEqual("CustomerId", relationship.ForeignColumnName, "The ForeignColumnName should be CustomerId.");
+            Assert.AreEqual("Order", relationship.TableName, "The relationship table name should be Order");
         }
 
     }
