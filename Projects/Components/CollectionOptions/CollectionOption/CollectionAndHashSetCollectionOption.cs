7ceac08e0f3615383577105ea175849b81dbfb1e
Laredo <laredoza@gmail.com>
2018-04-13 11:32:51 +0200

    Issue #36: Adding New Resharper Template

--- Projects/Components/CollectionOptions/CollectionOption/CollectionAndHashSetCollectionOption.cs
+++ Projects/Components/CollectionOptions/CollectionOption/CollectionAndHashSetCollectionOption.cs
@@ -1,13 +1,30 @@
-﻿using System.ComponentModel.Composition;
-using DotNetScaffolder.Components.Common.Contract;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CollectionAndHashSetCollectionOption.cs" company="DotnetScaffolder">
+//   MIT
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace DotNetScaffolder.Components.CollectionOption
 {
+    #region Usings
+
+    using System.ComponentModel.Composition;
+
+    using DotNetScaffolder.Components.Common.Contract;
+
+    #endregion
+
+    /// <summary>
+    /// The collection and hash set collection option.
+    /// </summary>
     [Export(typeof(ICollectionOption))]
     [ExportMetadata("NameMetaData", "Collection")]
     [ExportMetadata("ValueMetaData", "0BC1B0C4-1E41-9146-82CF-599181CE4410")]
     public class CollectionAndHashSetCollectionOption : ICollectionOption
     {
+        /// <summary>
+        /// The class name.
+        /// </summary>
         public string ClassName => "Hashtable";
     }
 }
