2fd621e2bed79af168007badaecd9f137013cc6b
Laredo Tirnanic <laredoza@gmail.com>
2018-04-19 19:53:29 +0200

    #41 Added Models folder back back

--- Projects/Presentation/Forms.Controls/Sources/UpdateModelsFromSourceForm.Designer.cs
+++ Projects/Presentation/Forms.Controls/Sources/UpdateModelsFromSourceForm.Designer.cs
@@ -30,14 +30,15 @@
         {
             this.BtnClose = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.updateModelsFromSourceUserControl1 = new UpdateModelsFromSourceUserControl();
+            this.updateModelsFromSourceUserControl1 = new DotNetScaffolder.Presentation.Forms.Controls.Sources.UpdateModelsFromSourceUserControl();
             this.SuspendLayout();
             // 
             // BtnClose
             // 
-            this.BtnClose.Location = new System.Drawing.Point(426, 402);
+            this.BtnClose.Location = new System.Drawing.Point(639, 618);
+            this.BtnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnClose.Name = "BtnClose";
-            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.Size = new System.Drawing.Size(112, 35);
             this.BtnClose.TabIndex = 1;
             this.BtnClose.Text = "Ok";
             this.BtnClose.UseVisualStyleBackColor = true;
@@ -45,9 +46,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(507, 402);
+            this.button3.Location = new System.Drawing.Point(760, 618);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 2;
             this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = true;
@@ -56,21 +58,23 @@
             // updateModelsFromSourceUserControl1
             // 
             this.updateModelsFromSourceUserControl1.DataSource = null;
-            this.updateModelsFromSourceUserControl1.Location = new System.Drawing.Point(9, 8);
-            this.updateModelsFromSourceUserControl1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.updateModelsFromSourceUserControl1.Location = new System.Drawing.Point(14, 12);
+            this.updateModelsFromSourceUserControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.updateModelsFromSourceUserControl1.Name = "updateModelsFromSourceUserControl1";
             this.updateModelsFromSourceUserControl1.SavePath = null;
-            this.updateModelsFromSourceUserControl1.Size = new System.Drawing.Size(608, 378);
+            this.updateModelsFromSourceUserControl1.Size = new System.Drawing.Size(912, 582);
             this.updateModelsFromSourceUserControl1.TabIndex = 3;
             // 
             // UpdateModelsFromSourceForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 436);
+            this.ClientSize = new System.Drawing.Size(938, 671);
             this.Controls.Add(this.updateModelsFromSourceUserControl1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.BtnClose);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
             this.Name = "UpdateModelsFromSourceForm";
             this.Text = "Update Tables From Source";
             this.ResumeLayout(false);
