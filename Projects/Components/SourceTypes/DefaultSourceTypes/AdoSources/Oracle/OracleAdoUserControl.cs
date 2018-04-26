15e9d2136ddb49f2fcb9e5b928a792b96f7b01e4
laredoza <laredoza@gmail.com>
2018-04-26 14:34:56 +0200

    #42 Oracle Connection is now being updated. Oracle Connection's can be tested

--- Projects/Components/SourceTypes/DefaultSourceTypes/AdoSources/Oracle/OracleAdoUserControl.cs
+++ Projects/Components/SourceTypes/DefaultSourceTypes/AdoSources/Oracle/OracleAdoUserControl.cs
@@ -136,6 +136,18 @@ namespace DotNetScaffolder.Components.SourceTypes.DefaultSourceTypes.AdoSources.
         /// </param>
         public void TestData(object parameters)
         {
+            Logger.Trace("Started TestData()");
+
+            if (this.SourceType.Test(this.options))
+            {
+                MessageBox.Show("Connected to Sql Server", "Test", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Unable to Connected to Sql Server", "Test", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            Logger.Trace("Completed TestData()");
         }
 
         /// <summary>
@@ -162,5 +174,10 @@ namespace DotNetScaffolder.Components.SourceTypes.DefaultSourceTypes.AdoSources.
         }
 
         #endregion
+
+        private void TxtConnection_TextChanged(object sender, EventArgs e)
+        {
+            this.options.ConnectionString = this.TxtConnection.Text;
+        }
     }
 }
