e7473b8cf8d69e005ce63f2353bb857446e6bc3c
Laredo Tirnanic <laredoza@gmail.com>
2018-05-06 19:25:54 +0200

    #79 Refactor

--- Projects/Components/Drivers/DefaultDrivers/EFCore/EFCoreSqlServerDriver.cs
+++ Projects/Components/Drivers/DefaultDrivers/EFCore/EFCoreSqlServerDriver.cs
@@ -4,7 +4,7 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace DotNetScaffolder.Components.Drivers.DefaultDrivers.EF7
+namespace DotNetScaffolder.Components.Drivers.DefaultDrivers.EFCore
 {
     #region Usings
 
