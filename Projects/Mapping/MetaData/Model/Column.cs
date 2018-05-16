77988edf604d3e4d823c7746267032589973a8e3
laredoza <laredoza@gmail.com>
2018-05-16 06:18:44 +0200

    #72 Added InvalidPrecisionGeneration

--- Projects/Mapping/MetaData/Model/Column.cs
+++ Projects/Mapping/MetaData/Model/Column.cs
@@ -45,25 +45,14 @@ namespace DotNetScaffolder.Mapping.MetaData.Model
         /// <summary>
         ///     Gets the column name with key indicator.
         /// </summary>
-        public string ColumnNameWithKeyIndicator
-        {
-            get
-            {
-                return string.Format("{0} {1}", this.IsPrimaryKey ? "* " : "   ", this.ColumnName);
-            }
-        }
+        public string ColumnNameWithKeyIndicator =>
+            string.Format("{0} {1}", this.IsPrimaryKey ? "* " : "   ", this.ColumnName);
 
         /// <summary>
         ///     Gets or sets the column order.
         /// </summary>
         [XmlAttribute("ColumnOrder")]
         public int ColumnOrder { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the c sharp data type.
-        /// </summary>
-        [XmlAttribute("DataType")]
-        public DomainDataType DomainDataType { get; set; }
 
         /// <summary>
         ///     Gets or sets the db type.
@@ -84,10 +73,23 @@ namespace DotNetScaffolder.Mapping.MetaData.Model
         public string Description { get; set; }
 
         /// <summary>
+        ///     Gets or sets the c sharp data type.
+        /// </summary>
+        [XmlAttribute("DataType")]
+        public DomainDataType DomainDataType { get; set; }
+
+        /// <summary>
         ///     Gets or sets the grid column width.
         /// </summary>
         [XmlAttribute("GridColumnWidth")]
         public int GridColumnWidth { get; set; } = 80;
+
+        /// <summary>
+        /// Invalid precision generation.
+        /// </summary>
+        [XmlIgnore]
+        public bool InValidPrecisionGeneration =>
+            this.DomainDataType == DomainDataType.Int16 || this.DomainDataType == DomainDataType.Int32;
 
         /// <summary>
         ///     Gets or sets a value indicating whether is primary key.
@@ -125,10 +127,7 @@ namespace DotNetScaffolder.Mapping.MetaData.Model
         [XmlAttribute("RenderToEntity")]
         public bool RenderToEntity
         {
-            get
-            {
-                return this.renderToEntity;
-            }
+            get => this.renderToEntity;
 
             set
             {
@@ -165,134 +164,48 @@ namespace DotNetScaffolder.Mapping.MetaData.Model
 
         #region Other Properties
 
-        // [XmlIgnore]
-        // private string DataTypeString
-        // {
-        // get
-        // {
-        // if ((!IsRequired) && (!IsPrimaryKey))
-        // {
-        // return NullableDataTypeString;
-        // }
-        // return RemappedDomainDataType;
-        // }
-        // }
-
         /// <summary>
         ///     Gets a value indicating whether nullable.
         /// </summary>
-        private bool Nullable
-        {
-            get
-            {
-                return !this.IsRequired && !this.IsPrimaryKey;
-            }
-        }
+        private bool Nullable => !this.IsRequired && !this.IsPrimaryKey;
 
         #endregion
 
         #region Public Methods And Operators
 
         /// <summary>
-        /// The clone.
+        ///     The clone.
         /// </summary>
         /// <returns>
-        /// The <see cref="object"/>.
+        ///     The <see cref="object" />.
         /// </returns>
         public object Clone()
         {
-            Column result = new Column
-                                {
-                                    ColumnName = this.ColumnName,
-                                    ColumnOrder = this.ColumnOrder,
-                                    DomainDataType = this.DomainDataType,
-                                    DbType = this.DbType,
-                                    DefaultFieldValue = this.DefaultFieldValue,
-                                    Description = this.Description,
-                                    GridColumnWidth = this.GridColumnWidth,
-                                    IsPrimaryKey = this.IsPrimaryKey,
-                                    IsRequired = this.IsRequired,
-                                    Length = this.Length,
-                                    LookupClassName = this.LookupClassName,
-                                    Precision = this.Precision,
-                                    RenderToEntity = this.renderToEntity,
-                                    RenderToView = this.RenderToView,
-                                    RenderToViewOrder = this.RenderToViewOrder,
-                                    Scale = this.Scale
-                                };
+            var result = new Column
+                             {
+                                 ColumnName = this.ColumnName,
+                                 ColumnOrder = this.ColumnOrder,
+                                 DomainDataType = this.DomainDataType,
+                                 DbType = this.DbType,
+                                 DefaultFieldValue = this.DefaultFieldValue,
+                                 Description = this.Description,
+                                 GridColumnWidth = this.GridColumnWidth,
+                                 IsPrimaryKey = this.IsPrimaryKey,
+                                 IsRequired = this.IsRequired,
+                                 Length = this.Length,
+                                 LookupClassName = this.LookupClassName,
+                                 Precision = this.Precision,
+                                 RenderToEntity = this.renderToEntity,
+                                 RenderToView = this.RenderToView,
+                                 RenderToViewOrder = this.RenderToViewOrder,
+                                 Scale = this.Scale
+                             };
 
-            if (result.Table != null)
-            {
-                result.Table = this.Table.Clone() as Table;
-            }
+            if (result.Table != null) result.Table = this.Table.Clone() as Table;
 
             return result;
         }
 
         #endregion
-
-        // }
-        // }
-        // return RemappedDomainDataType;
-        // default:
-        // private GridViewControl _gridViewControlType = GridViewControl.Unknown;
-        // break;
-        // return string.Format("{0}?", RemappedDomainDataType);
-        // case "GUID":
-        // case "DATETIME":
-        // case "DOUBLE":
-        // case "DECIMAL":
-        // case "LONG":
-        // case "INT":
-        // {
-        // switch (RemappedDomainDataType.ToUpper())
-        // {
-        // get
-        // {
-        // }
-        // get { return Table == null ? null : Table.TableCollection.Domain; }
-        // {
-        // public AutomationDomain Domain
-
-        // [XmlIgnore]
-        // private string NullableDataTypeString
-
-        // public void RaisePropertyChanged([CallerMemberName]string prop = "")
-        // }
-        // {
-        // if (PropertyChanged != null)
-        // PropertyChanged(this, new PropertyChangedEventArgs(prop));
-        // }
-
-        // public void RaisePropertyChanged<T>(Expression<Func<T>> prop)
-        // {
-        // if (PropertyChanged != null)
-        // {
-        // var member = prop.Body as MemberExpression;
-        // if (member == null) return;
-
-        // var propInfo = member.Member as PropertyInfo;
-        // if (propInfo == null) return;
-
-        // PropertyChanged(this, new PropertyChangedEventArgs(propInfo.Name));
-        // }
-        // }
-
-        // private GridViewControl SetControlType(string dataType)
-        // {
-        // switch (dataType.ToUpper())
-        // {
-        // case "DATETIME":
-        // return GridViewControl.DateTime;
-        // break;
-        // case "INT":
-        // case "DECIMAL":
-        // return GridViewControl.Number;
-        // break;
-        // default:
-        // return GridViewControl.TextBox;
-        // break;
-        // }
-        // }
     }
 }
