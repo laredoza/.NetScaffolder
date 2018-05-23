dde3a7d6a7e6158264e7906ca8a1d01689d0172f
Laredo Tirnanic <laredoza@gmail.com>
2018-05-23 14:34:47 +0200

    Merged Changes

--- Projects/Components/SourceTypes/DefaultSourceTypes/Edmxs/EdmxSourceType.cs
+++ Projects/Components/SourceTypes/DefaultSourceTypes/Edmxs/EdmxSourceType.cs
@@ -10,7 +10,7 @@ namespace DotNetScaffolder.Components.SourceTypes.DefaultSourceTypes.Edmxs
 
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.Composition;
+    using System.Composition;
     using System.IO;
     using System.Linq;
     using System.Windows.Forms;
