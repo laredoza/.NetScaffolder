67cb922c57f1cd8c6fc1e2d724100e9cc5b1610c
Laredo Tirnanic <laredoza@gmail.com>
2018-05-27 10:35:39 +0200

    #99 Added Proxy

---
+++ Generated/Presentation/Angular/BankingAngular/Api/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            // https://medium.com/@levifuller/building-an-angular-application-with-asp-net-core-in-visual-studio-2017-visualized-f4b163830eaa
+            return new string[] { "Item 1", "Item 2" };
+        }
+    }
+}
