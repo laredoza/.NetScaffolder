5726deef59ecdf2f64a0e78febb5ce1055493881
Laredo Tirnanic <laredoza@gmail.com>
2018-06-06 16:25:33 +0200

    #73 Loading Edmx Source

---
+++ Tests/CodeGenerator/Project/ApplicationService/Differences/BaseApplicationTableCollectionDifferenceUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DotNetScaffolder.Mapping.ApplicationServices.Differences;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetScaffolder.Test.Project.ApplicationService.Differences
+{
+    using DotNetScaffolder.Components.Common.Contract;
+
+    /// <summary>
+    /// The base application table collection difference unit test.
+    /// </summary>
+    [TestClass]
+    public class BaseApplicationTableCollectionDifferenceUnitTest
+    {
+        /// <summary>
+        /// Gets or sets the difference.
+        /// </summary>
+        public IApplicationTableCollectionDifference Difference { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source type.
+        /// </summary>
+        public ISourceType SourceType { get; set; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
