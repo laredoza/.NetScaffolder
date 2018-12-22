4a2f00c854c988f92b09de84228e3e35d2a8da96
Laredo Tirnanic <laredoza@gmail.com>
2018-12-22 17:08:38 +0200

    #123 Started adding generator & template

--- Templates/Generated/Dal/Domain/Dotnet/Services/WebApi/Default/Program.cs
+++ Templates/Generated/Dal/Domain/Dotnet/Services/WebApi/Default/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Security
+namespace Default
 {
     public class Program
     {
